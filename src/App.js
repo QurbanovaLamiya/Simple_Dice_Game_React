828da34fab3d533e962e5e74fd7038644b9b6e38
QurbanovaLamiya <lamiyaqurbanova121@gmail.com>
2022-10-17 19:59:56 +0400

    Update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component, Fragment } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Dice</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
